97ca248f86bd669164cfbfde62f5eaf74194229e
vigneshdfc <vigneshdfc02@gmail.com>
2023-05-22 18:30:53 +0530

    chore:edit cureection on element section

--- src/ui/layout/List.js
+++ src/ui/layout/List.js
@@ -12,22 +12,21 @@ export const ListItem = styled.li`
   min-height: 24px;
   padding: 13px 24px;
   align-items: center;
-  color: ${props => props.theme.text};
-
+  color: ${props => (props.selected || props.focused ? props.theme.text : props.theme.text2)};
+  background: ${props => props.theme.grayClr};
   // background-color: ${props => (props.selected ? props.theme.selected : props.theme.panel2)};
 
   :nth-child(odd) {
     // background-color: ${props => (props.selected ? props.theme.selected : props.theme.panel)};
   }
-
-  :hover,
+  :hover, 
   :focus {
-    background-color: ${props => (props.selected ? props.theme.blueHover : props.theme.hover)};
+    background-color: ${props => (props.selected ? props.theme.emptyBoxClr : props.theme.hover)};
     color: ${props => props.theme.text};
   }
 
   :active {
-    background-color: ${props => props.theme.bluePressed};
+    // background-color: ${props => props.theme.bluePressed};
     color: ${props => props.theme.text};
   }
 `;
