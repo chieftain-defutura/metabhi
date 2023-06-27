541774adddc1ca1915a098e3dcbbd1eeaef63d23
Lokendra Soni <lokendrasoni10@gmail.com>
2023-06-27 09:45:52 +0530

    feat: added chains

--- src/ui/connectors.js
+++ src/ui/connectors.js
@@ -1,5 +1,5 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
+import { InjectedConnector } from "@web3-react/injected-connector"
 
 export const Injected = new InjectedConnector({
-  supportedChainIds: [356256156]
-});
+  supportedChainIds: [356256156, 997, 8082, 43113, 1284]
+})
